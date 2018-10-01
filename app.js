568f555f9f88b0db73254d88e254c50939491ce2
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-10-01 11:58:48 -0700

    Render Grocery List with dynamic style properties
    - Bold on hover
    - Line-through on click

--- app.js
+++ app.js
@@ -1,1 +1,72 @@
 // TODO
+
+var Apples = () => (
+    <li>Apples</li>
+);
+
+var Oranges = () => (
+    <li>Oranges</li>
+);
+
+
+// var GroceryListItem = (props) => (
+//     <ul>
+//         <li>{props.fruits[0]}</li>
+//         <li>{props.fruits[1]}</li>
+//     </ul>
+// )
+
+class GroceryListItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            done: false,
+            hover: false
+        }
+    }
+
+    onListItemClick() {
+        this.setState({
+            done: !this.state.done
+        })
+    }
+
+    onListItemHover() {
+        this.setState({
+            hover: !this.state.hover
+        })
+    }
+
+    render() {
+        var style = {
+            textDecoration: this.state.done ? 'line-through' : 'none',
+            fontWeight: this.state.hover ? '700' : '200'
+        }
+
+        return (
+            <li style={style} 
+            onClick={this.onListItemClick.bind(this)}
+            onMouseEnter={this.onListItemHover.bind(this)}
+            onMouseLeave={this.onListItemHover.bind(this)}>{this.props.fruit}</li>
+        );
+    }
+}
+
+var GroceryList = (props) => (
+    <ul>
+        {props.fruits.map(fruit =>
+            <GroceryListItem fruit={fruit} />
+        )}
+    </ul>
+);
+
+var App = () => (
+    <div>
+        <h2>My Grocery List</h2>
+        <GroceryList fruits={['apple', 'orange']} />
+    </div>
+);
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
